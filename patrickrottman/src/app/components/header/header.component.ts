17b46af80a62142e8f333770870c6ef903c4dbf8
Patrick Rottman <prottman@drivecentric.com>
2024-11-21 18:13:11 -0600

    v3

--- patrickrottman/src/app/components/header/header.component.ts
+++ patrickrottman/src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -14,7 +13,6 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
